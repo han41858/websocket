20b95aa96caf1662537c6250a13bbeaa91bdf66c
han41858 <han41858@gmail.com>
2021-01-07 14:54:35 +0900

    기본 로직 추가

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,21 @@
-console.log('server');
+import * as ws from 'ws';
+
+console.log('server start');
+
+
+const wss = new ws.Server({
+	port : 3001
+});
+
+
+wss.on('connection', (ws) => {
+	console.log('\nws connection');
+
+	ws.on('message', (msg) => {
+		console.log(`ws message [${ typeof msg }] :`, msg);
+	});
+
+	ws.on('close', () => {
+		console.log('ws close\n');
+	});
+});
--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { tap } from 'rxjs/operators';
+import { webSocket } from 'rxjs/webSocket';
+
 
 @Component({
 	selector : 'app-root',
 	templateUrl : './app.component.html',
 	styleUrls : ['./app.component.styl']
 })
-export class AppComponent {
-	title = 'ws';
+export class AppComponent implements OnInit {
+
+	constructor () {
+	}
+
+	ngOnInit () : void {
+
+	}
+
+	send1Message () : void {
+		console.log('sendMessage()');
+
+		const subject = webSocket('ws://localhost:3001');
+
+		subject.pipe(
+			tap((msg) => {
+				console.log('on message :', msg);
+			})
+		);
+
+		subject.subscribe();
+
+
+		subject.next({
+			msg : 'hello'
+		});
+
+		subject.complete();
+	}
+
 }
